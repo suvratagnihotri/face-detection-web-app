6c8504f40f3d195fca3d8f90013ce13bb78cf995
suvratagnihotri <suvrat72000@gmail.com>
2023-06-01 22:16:55 +0530

    Update commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { useEffect, useRef, useState } from 'react';
 import Webcam from 'react-webcam';
-import * as faceapi from 'face-api.js';
-import { loadModels, detectFaces } from './faceDetection';
-
+import { useOpenCv } from 'opencv-react'; // Import the useOpenCv hook
+import classifierXml from './haarcascade_frontalface_default.xml';
 
 const App = () => {
   const videoRef = useRef(null);
   const canvasRef = useRef(null);
   const [isCameraOn, setIsCameraOn] = useState(false);
   const [isImageCaptured, setIsImageCaptured] = useState(false);
+  const [classifierData, setClassifierData] = useState(null);
+  const { cv } = useOpenCv(); // Access the OpenCV instance
 
   const startCamera = () => {
     setIsCameraOn(true);
@@ -33,49 +34,59 @@ const App = () => {
     canvasRef.current = canvasElement;
   };
 
-  const detectFaces = async () => {
-    try {
-      await loadModels(); // Load face-api.js models
-  
-      const videoElement = videoRef.current.video;
-      const canvasElement = document.createElement('canvas');
-      const context = canvasElement.getContext('2d');
-  
-      // Set canvas dimensions to match the video stream
-      canvasElement.width = videoElement.videoWidth;
-      canvasElement.height = videoElement.videoHeight;
-  
-      // Draw the current video frame onto the canvas
-      context.drawImage(videoElement, 0, 0, canvasElement.width, canvasElement.height);
-  
-      setIsCameraOn(false);
-      videoElement.srcObject.getTracks().forEach((track) => track.stop());
-  
-      setIsImageCaptured(true);
-      canvasRef.current = canvasElement;
-  
-      const image = await faceapi.fetchImage(canvasElement.toDataURL());
-      const detectedFaces = await detectFaces(image); // Perform face detection using face-api.js
-  
-      // Draw bounding boxes around detected faces
-      const resultImage = faceapi.createCanvasFromMedia(image);
-      faceapi.draw.drawDetections(resultImage, detectedFaces);
-  
-      const resultContainer = document.createElement('div');
-      resultContainer.classList.add('result-container');
-      resultContainer.appendChild(resultImage);
-  
-      document.body.appendChild(resultContainer);
-    } catch (error) {
-      console.log('Face detection error:', error);
+  const detectFaces = () => {
+    const imgElement = canvasRef.current;
+    const src = cv.imread(imgElement);
+    
+    // Convert the image to gray scale for face detection
+    const gray = new cv.Mat();
+    cv.cvtColor(src, gray, cv.COLOR_RGBA2GRAY, 0);
+
+    // Load the pre-trained face detection classifier
+    const classifier = new cv.CascadeClassifier();
+    let location = cv.HAAR_FRONTALFACE_DEFAULT;
+    // classifier.load(location); 
+    classifier.load(classifierXml);
+
+    // Detect faces in the image
+    const faces = new cv.RectVector();
+    classifier.detectMultiScale(gray, faces);
+
+    // Draw rectangles around the detected faces
+    for (let i = 0; i < faces.size(); i++) {
+      const faceRect = faces.get(i);
+      const point1 = new cv.Point(faceRect.x, faceRect.y);
+      const point2 = new cv.Point(faceRect.x + faceRect.width, faceRect.y + faceRect.height);
+      cv.rectangle(src, point1, point2, [255, 0, 0, 255]);
     }
+
+    // Display the result
+    cv.imshow(canvasRef.current, src);
+    
+    // Clean up
+    src.delete();
+    gray.delete();
+    classifier.delete();
+    faces.delete();
   };
-  
 
   useEffect(() => {
     console.log('Initializing camera...');
     startCamera();
     console.log('Camera initialized.');
+    const loadClassifier = async () => {
+      try {
+        const response = await fetch('/haarcascade_frontalface_default.xml');
+        const xmlData = await response.text();
+        const blob = new Blob([xmlData], { type: 'application/xml' });
+        const url = URL.createObjectURL(blob);
+        setClassifierData(url);
+      } catch (error) {
+        console.error('Error loading XML file:', error);
+      }
+    };
+  
+    loadClassifier();
   }, []);
 
   return (
@@ -88,7 +99,7 @@ const App = () => {
       )}
       {isImageCaptured ? (
         <div className="camera-container">
-          <img className="video" src={canvasRef.current?.toDataURL()} alt="Captured" />
+          <img className="video" src={canvasRef.current?.toDataURL()} alt="Captured" crossOrigin="anonymous" />
         </div>
       ) : (
         <div className="button-container">
--- src/faceDetection.js
+++ src/faceDetection.js
@@ -1,7 +1,7 @@
 import * as faceapi from 'face-api.js';
 
 export const loadModels = async () => {
-  const MODEL_URL = '/models'; // Path to face-api.js models
+  const MODEL_URL = "/home/ginger/Desktop/poc_web_image_processing/react-pro/face-detection/public/models"; // Path to face-api.js models
 
   await Promise.all([
     faceapi.nets.ssdMobilenetv1.loadFromUri(MODEL_URL),
